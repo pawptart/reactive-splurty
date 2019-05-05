bb3d4d2730014dad4ff1b910f8df82ad5f03271b
Tyler Porter <tyler.b.porter@gmail.com>
2019-05-05 01:06:24 +0000

    Added CSS to quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -12,12 +12,12 @@ class Quote extends React.Component {
 		return (
 			<div className="quote-container">
 				<div className="quote-id">
-					SPLURTY / No. { quote.id }  
+					<span className="id-span">SPLURTY / No. { quote.id }</span> Here's the best user-generated <span className="underline">motivation</span> you can get! 
 				</div>
 				<div className="quote-saying">
 					{ quote.saying }
 				</div>
-				<div className="quote.author">
+				<div className="quote-author">
 					- { quote.author }
 				</div>				
 			</div>
