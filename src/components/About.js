44b2aeb2115d3e15b7f8327b93407c7dea6adc56
Tyler Porter <tyler.b.porter@gmail.com>
2019-05-08 00:35:42 +0000

    Styled about page

--- src/components/About.js
+++ src/components/About.js
@@ -13,23 +13,28 @@ class About extends React.Component {
 					<br /><br/>
 					Splurty was originally a Ruby on Rails project turned ReactJS learning experience. Splurty started 
 					out as Tyler's first foray into Ruby on Rails. He enjoyed it so much he decided to rebuild it as 
-					a SPA web app using Ruby on Rails -- what he calls "Reactive Splurty".
+					a SPA web app using ReactJS to call a Ruby on Rails API -- what he calls "Reactive Splurty".
 				</div>
 				<br /><br />
 				<div className="learn-coding">
 					If you want to learn how to code - start <a href="https://online.bootcampspot.com/">here</a>.
 				</div>
-				<img src={me} alt="Tyler Porter Profile Picture"></img>
-				<h2 className="name">
-					Tyler Porter
-				</h2>
-				<div className="about-me">
-					Tyler Porter is a junior web developer by night and a technology sales representative by day. 
-					He has always loved working with new technologies and is eager to make the jump to web development 
-					after graduating from Vanderbilt's bootcamp in August. Currently, he is working on learning 
-					full-stack web development and has experience with HTML/CSS/JS and is eager to pick up new skills 
-					(<a href="https://github.com/pawptart/reactive-splurty">like learning Ruby on Rails and ReactJS 
-					by building Splurty!)</a>
+				<br />
+				<div className="profile-container">
+					<img src={me} alt="Tyler Porter Profile Picture"></img>
+					<div className="about-me-container">
+						<h2 className="name">
+							Tyler Porter
+						</h2>
+						<div className="about-me">
+							Tyler Porter is a junior web developer by night and a technology sales representative by day. 
+							He has always loved working with new technologies and is eager to make the jump to web development 
+							after graduating from Vanderbilt's bootcamp in August. Currently, he is working on learning 
+							full-stack web development and has experience with HTML/CSS/JS and is eager to pick up new skills 
+							(<a href="https://github.com/pawptart/reactive-splurty">like learning Ruby on Rails and ReactJS 
+							by building Splurty!</a>)
+						</div>
+					</div>
 				</div>
 			</div>
 		);
